cc24198818a0f50f2f57dd968e94c3392b6e1541
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-04-28 18:10:27 +0100

    refactor: configure the database using .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,29 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersModule } from "./users/users.module";
-import { ProjectsModule } from "./projects/projects.module";
-import { TeamsModule } from "./teams/teams.module";
-import { TasksModule } from "./tasks/tasks.module";
-import { ProgressModule } from "./progress/progress.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TeamsModule } from './teams/teams.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ProgressModule } from './progress/progress.module';
 import { InvitationsModule } from './invitations/invitations.module';
 import { EventsModule } from './events/events.module';
 import { CommentsModule } from './comments/comments.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "Farah",
-      database: "postgres",
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [
-         __dirname + '/**/*.entity{.ts,.js}'
+        __dirname + '/**/*.entity{.ts,.js}',
       ],
       synchronize: true,
     }),
