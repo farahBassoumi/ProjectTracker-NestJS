74566b02633a5a938330786e7a5e7d4403454424
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-04-29 23:08:03 +0100

    fix: extend CrudService by CommentsService

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { CrudService } from '../common/crud/crud.service';
+import { Comment } from './entities/comment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
-  }
-
-  findAll() {
-    return `This action returns all comments`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
-  }
-
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+export class CommentsService extends CrudService<Comment> {
+  constructor(
+    @InjectRepository(Comment)
+    cvsRepository: Repository<Comment>,
+  ) {
+    super(cvsRepository);
   }
 }
