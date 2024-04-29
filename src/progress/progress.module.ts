7f2c5dd340161a73c2b0c5148429ea67ccdb827c
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-04-30 00:05:30 +0100

    fix: add TypeORM forFeature to the modules

--- src/progress/progress.module.ts
+++ src/progress/progress.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProgressService } from './progress.service';
 import { ProgressController } from './progress.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Progress } from './entities/progress.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Progress])],
   controllers: [ProgressController],
   providers: [ProgressService],
 })
