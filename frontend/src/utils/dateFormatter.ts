ed727104e59afb745112dd182b8758576b368384
Zeineb Ben Abdallah <zeineb.benabdallah@insat.ucar.tn>
2024-05-27 04:42:45 +0100

    fix: format dates

---
+++ frontend/src/utils/dateFormatter.ts
@@ -0,0 +1,38 @@
+export default function dateFormatter(date: Date): string {
+  const now = new Date();
+  const oneDayInMillis = 24 * 60 * 60 * 1000;
+  const dateStartOfDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+  );
+  const nowStartOfDay = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+  );
+
+  const timeDifference = nowStartOfDay.getTime() - dateStartOfDay.getTime();
+
+  if (timeDifference < oneDayInMillis && timeDifference >= 0) {
+    // Format as "Today, HH:MM AM/PM"
+    const timeOptions: Intl.DateTimeFormatOptions = {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    };
+    return `Today, ${date.toLocaleTimeString('en-US', timeOptions)}`;
+  } else if (
+    timeDifference < 2 * oneDayInMillis &&
+    timeDifference >= oneDayInMillis
+  ) {
+    return 'Yesterday';
+  } else {
+    const dateOptions: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    };
+    return date.toLocaleDateString('en-US', dateOptions);
+  }
+}
